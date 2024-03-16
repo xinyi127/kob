0645f59a8113b0073f69d4687cd14e3c41dc7116
xinyi127 <1224179212@qq.com>
2024-03-16 21:44:00 +0800

    搭建项目骨架

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,19 +3,20 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<String> getBotInfo() {
-        List<String> list = new LinkedList<>();
-        list.add("sword");
-        list.add("tiger");
-        list.add("apple");
-        return list;
+    public Map<String, String> getBotInfo() {
+        Map<String, String> bot = new HashMap<>();
+        bot.put("name", "tiger");
+        bot.put("rating", "1500");
+        return bot;
     }
 }
