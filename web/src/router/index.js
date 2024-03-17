e85c31cbb3cf8796ddc28c125ef057e44be134a7
xinyi127 <1224179212@qq.com>
2024-03-18 00:34:21 +0800

    实现导航栏

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import NotFound from '@/views/error/NotFound'
+import PkIndexView from '@/views/pk/PkIndexView'
+import RanklistIndexView from '@/views/ranklist/RanklistIndexView'
+import RecordIndexView from '@/views/record/RecordIndexView'
+import UserBotIndexView from '@/views/user/bot/UserBotIndexView'
 
+// 页面路由，URL 从上往下匹配
 const routes = [
-
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/"
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFound,
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RanklistIndexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/"
+  }
 ]
 
 const router = createRouter({
