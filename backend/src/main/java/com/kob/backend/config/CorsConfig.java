13307439f289168f33348725d81cd413e86150a0
xinyi127 <1224179212@qq.com>
2024-03-26 19:49:23 +0800

    配置 MySQL，更改 Java、JDK、Spring Boot版本

--- backend/src/main/java/com/kob/backend/config/CorsConfig.java
+++ backend/src/main/java/com/kob/backend/config/CorsConfig.java
@@ -2,9 +2,9 @@ package com.kob.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Configuration
