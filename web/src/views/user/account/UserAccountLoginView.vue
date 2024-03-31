415ee3badf20c5ee3f5a337ee53337aaae6e2aec
xinyi127 <1224179212@qq.com>
2024-03-31 18:19:21 +0800

    实现注册页面和登录信息持久化

--- web/src/views/user/account/UserAccountLoginView.vue
+++ web/src/views/user/account/UserAccountLoginView.vue
@@ -1,5 +1,5 @@
 <template>
-    <ContentField>
+    <ContentField v-if="!$store.state.user.pulling_info">
         <div class="row justify-content-md-center">
             <div class="col-3">
                 <form @submit.prevent="login">
@@ -36,6 +36,22 @@ export default {
         let password = ref('');
         let error_message = ref('');
 
+        const jwt_token = localStorage.getItem("jwt_token");
+        if (jwt_token) {
+            store.commit("updateToken", jwt_token);
+            store.dispatch("getinfo", {
+                success() {
+                    router.push({ name: "home" });
+                    store.commit("updatepullingInfo", false);
+                },
+                error() {
+                    store.commit("updatepullingInfo", false);
+                }
+            })
+        } else {
+            store.commit("updatepullingInfo", false);
+        }
+
         const login = () => {
             error_message.value = "";
             store.dispatch("login", {
@@ -45,7 +61,6 @@ export default {
                     store.dispatch("getinfo", {
                         success() {
                             router.push({ name: 'home' });
-                            console.log(store.state.user);
                         }
                     })
                 },
