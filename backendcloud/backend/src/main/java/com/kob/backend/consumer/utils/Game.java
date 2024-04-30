c7504d7035fcc3f2f529a49bbc72073434e6c55d
xinyi127 <1224179212@qq.com>
2024-04-30 15:14:51 +0800

    实现Bot代码的执行

--- backendcloud/backend/src/main/java/com/kob/backend/consumer/utils/Game.java
+++ backendcloud/backend/src/main/java/com/kob/backend/consumer/utils/Game.java
@@ -1,18 +1,17 @@
 package com.kob.backend.consumer.utils;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.kob.backend.consumer.WebSocketServer;
-import com.kob.backend.mapper.RecordMapper;
+import com.kob.backend.pojo.Bot;
 import com.kob.backend.pojo.Record;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.locks.ReentrantLock;
-
-import static javafx.scene.input.KeyCode.R;
 
 public class Game extends Thread {
     private final Integer rows;
@@ -24,16 +23,36 @@ public class Game extends Thread {
     private Integer nextStepA = null;
     private Integer nextStepB = null;
     private ReentrantLock lock = new ReentrantLock();
-    private String status = "playing"; // playing -> finished
-    private String loser = ""; // all: 平局, A: A输, B: B输
+    private String status = "playing";  // playing -> finished
+    private String loser = "";  // all: 平局，A: A输，B: B输
+    private final static String addBotUrl = "http://127.0.0.1:3002/bot/add/";
 
-    public Game(Integer rows, Integer cols, Integer inner_walls_count, Integer idA, Integer idB) {
+    public Game(
+            Integer rows,
+            Integer cols,
+            Integer inner_walls_count,
+            Integer idA,
+            Bot botA,
+            Integer idB,
+            Bot botB
+    ) {
         this.rows = rows;
         this.cols = cols;
         this.inner_walls_count = inner_walls_count;
         this.g = new int[rows][cols];
-        playerA = new Player(idA, rows - 2, 1, new ArrayList<>());
-        playerB = new Player(idB, 1, cols - 2, new ArrayList<>());
+
+        Integer botIdA = -1, botIdB = -1;
+        String botCodeA = "", botCodeB = "";
+        if (botA != null) {
+            botIdA = botA.getId();
+            botCodeA = botA.getContent();
+        }
+        if (botB != null) {
+            botIdB = botB.getId();
+            botCodeB = botB.getContent();
+        }
+        playerA = new Player(idA, botIdA, botCodeA, rows - 2, 1, new ArrayList<>());
+        playerB = new Player(idB, botIdB, botCodeB, 1, cols - 2, new ArrayList<>());
     }
 
     public Player getPlayerA() {
@@ -66,14 +85,14 @@ public class Game extends Thread {
         return g;
     }
 
-    private boolean cherk_connectivity(int sx, int sy, int tx, int ty) {
+    private boolean check_connectivity(int sx, int sy, int tx, int ty) {
         if (sx == tx && sy == ty) return true;
         g[sx][sy] = 1;
 
-        for (int i = 0; i < 4; i ++) {
+        for (int i = 0; i < 4; i ++ ) {
             int x = sx + dx[i], y = sy + dy[i];
             if (x >= 0 && x < this.rows && y >= 0 && y < this.cols && g[x][y] == 0) {
-                if (cherk_connectivity(x, y, tx, ty)) {
+                if (check_connectivity(x, y, tx, ty)) {
                     g[sx][sy] = 0;
                     return true;
                 }
@@ -84,57 +103,85 @@ public class Game extends Thread {
         return false;
     }
 
-    private boolean draw() { // 画地图
-        for (int i = 0; i < this.rows; i ++) {
-            for (int j = 0; j < this.cols; j ++) {
+    private boolean draw() {  // 画地图
+        for (int i = 0; i < this.rows; i ++ ) {
+            for (int j = 0; j < this.cols; j ++ ) {
                 g[i][j] = 0;
             }
         }
 
-        for (int r = 0; r < this.rows; r ++) {
+        for (int r = 0; r < this.rows; r ++ ) {
             g[r][0] = g[r][this.cols - 1] = 1;
         }
-        for (int c = 0; c < this.cols; c ++) {
+        for (int c = 0; c < this.cols; c ++ ) {
             g[0][c] = g[this.rows - 1][c] = 1;
         }
 
         Random random = new Random();
-        for (int i = 0; i < this.inner_walls_count / 2; i ++) {
-            for (int j = 0; j < 1000; j ++) {
+        for (int i = 0; i < this.inner_walls_count / 2; i ++ ) {
+            for (int j = 0; j < 1000; j ++ ) {
                 int r = random.nextInt(this.rows);
                 int c = random.nextInt(this.cols);
 
-                if (g[r][c] == 1 || g[this.rows - 1 - r][this.cols - 1 - c] == 1) {
+                if (g[r][c] == 1 || g[this.rows - 1 - r][this.cols - 1 - c] == 1)
                     continue;
-                }
-                if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2) {
+                if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2)
                     continue;
-                }
 
                 g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = 1;
                 break;
             }
         }
 
-        return cherk_connectivity(this.rows - 2, 1, 1, this.cols - 2);
+        return check_connectivity(this.rows - 2, 1, 1, this.cols - 2);
     }
 
     public void createMap() {
-        for (int i = 0; i < 1000; i ++) {
-            if (draw()) {
+        for (int i = 0; i < 1000; i ++ ) {
+            if (draw())
                 break;
-            }
         }
     }
 
-    private boolean nextStep() { // 等待两名玩家的下一步操作
+    private String getInput(Player player) {  // 将当前的局面信息，编码成字符串
+        Player me, you;
+        if (playerA.getId().equals(player.getId())) {
+            me = playerA;
+            you = playerB;
+        } else {
+            me = playerB;
+            you = playerA;
+        }
+
+        return getMapString() + "#" +
+                me.getSx() + "#" +
+                me.getSy() + "#(" +
+                me.getStepsString() + ")#" +
+                you.getSx() + "#" +
+                you.getSy() + "#(" +
+                you.getStepsString() + ")";
+    }
+
+    private void sendBotCode(Player player) {
+        if (player.getBotId().equals(-1)) return;  // 亲自出马，不需要执行代码
+        MultiValueMap<String, String> data = new LinkedMultiValueMap<>();
+        data.add("user_id", player.getId().toString());
+        data.add("bot_code", player.getBotCode());
+        data.add("input", getInput(player));
+        WebSocketServer.restTemplate.postForObject(addBotUrl, data, String.class);
+    }
+
+    private boolean nextStep() {  // 等待两名玩家的下一步操作
         try {
             Thread.sleep(200);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        for (int i = 0; i < 50; i ++) {
+        sendBotCode(playerA);
+        sendBotCode(playerB);
+
+        for (int i = 0; i < 50; i ++ ) {
             try {
                 Thread.sleep(100);
                 lock.lock();
@@ -158,26 +205,22 @@ public class Game extends Thread {
     private boolean check_valid(List<Cell> cellsA, List<Cell> cellsB) {
         int n = cellsA.size();
         Cell cell = cellsA.get(n - 1);
-        if (g[cell.x][cell.y] == 1) {
-            return false;
+        if (g[cell.x][cell.y] == 1) return false;
+
+        for (int i = 0; i < n - 1; i ++ ) {
+            if (cellsA.get(i).x == cell.x && cellsA.get(i).y == cell.y)
+                return false;
         }
 
-        for (int i = 0; i < n - 1; i ++) {
-            if (cellsA.get(i).x == cell.x && cellsA.get(i).y == cell.y) {
+        for (int i = 0; i < n - 1; i ++ ) {
+            if (cellsB.get(i).x == cell.x && cellsB.get(i).y == cell.y)
                 return false;
-            }
-        }
-
-        for (int i = 0; i < n - 1; i ++) {
-            if (cellsB.get(i).x == cell.x && cellsB.get(i).y == cell.y) {
-                return false;
-            }
         }
 
         return true;
     }
 
-    private void judge() { // 判断两名玩家下一步操作是否合法
+    private void judge() {  // 判断两名玩家下一步操作是否合法
         List<Cell> cellsA = playerA.getCells();
         List<Cell> cellsB = playerB.getCells();
 
@@ -190,7 +233,7 @@ public class Game extends Thread {
                 loser = "all";
             } else if (!validA) {
                 loser = "A";
-            } else if (!validB) {
+            } else {
                 loser = "B";
             }
         }
@@ -203,7 +246,7 @@ public class Game extends Thread {
             WebSocketServer.users.get(playerB.getId()).sendMessage(message);
     }
 
-    private void sendMove() { // 向两个client传递移动信息
+    private void sendMove() {  // 向两个Client传递移动信息
         lock.lock();
         try {
             JSONObject resp = new JSONObject();
@@ -219,15 +262,15 @@ public class Game extends Thread {
 
     private String getMapString() {
         StringBuilder res = new StringBuilder();
-        for (int i = 0; i < rows; i ++) {
-            for (int j = 0; j < cols; j ++) {
+        for (int i = 0; i < rows; i ++ ) {
+            for (int j = 0; j < cols; j ++ ) {
                 res.append(g[i][j]);
             }
         }
         return res.toString();
     }
 
-    private void saveToDataBase() {
+    private void saveToDatabase() {
         Record record = new Record(
                 null,
                 playerA.getId(),
@@ -246,18 +289,18 @@ public class Game extends Thread {
         WebSocketServer.recordMapper.insert(record);
     }
 
-    private void sendResult() { // 向两个client公布结果
+    private void sendResult() {  // 向两个Client公布结果
         JSONObject resp = new JSONObject();
         resp.put("event", "result");
         resp.put("loser", loser);
-        saveToDataBase();
+        saveToDatabase();
         sendAllMessage(resp.toJSONString());
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 1000; i ++) {
-            if (nextStep()) { // 是否获取了两条蛇的下一步操作
+        for (int i = 0; i < 1000; i ++ ) {
+            if (nextStep()) {  // 是否获取了两条蛇的下一步操作
                 judge();
                 if (status.equals("playing")) {
                     sendMove();
--- backendcloud/backend/src/main/java/com/kob/backend/consumer/utils/Player.java
+++ backendcloud/backend/src/main/java/com/kob/backend/consumer/utils/Player.java
@@ -12,11 +12,13 @@ import java.util.List;
 @NoArgsConstructor
 public class Player {
     private Integer id;
+    private Integer botId;  // -1表示亲自出马，否则表示用AI打
+    private String botCode;
     private Integer sx;
     private Integer sy;
     private List<Integer> steps;
 
-    private boolean check_tail_increasing(int step) { // 检验当前回合，蛇的长度是否增加
+    private boolean check_tail_increasing(int step) {  // 检验当前回合，蛇的长度是否增加
         if (step <= 10) return true;
         return step % 3 == 1;
     }
